5461b3f172c228b6487df2dad09b271b07fa5a1a
Samsil Arefin <samsilarefin000@gmail.com>
2016-08-06 00:13:34 +0600

    Add files via upload

---
+++ Can You Count.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,n,T,t,i;
+    cin>>T;
+    for(t=1;t<=t;t++)
+    {
+        set<long long>res;
+        cin>>n;
+        for(i=0;i<n;i++)
+        {
+            cin>>a;
+            res.insert(a);
+
+        }
+        cout<<"Case "<<t<<": "<<res.size()<<endl;
+    }
+    return 0;
+}
